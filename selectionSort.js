fd85916503b0f72682e3a78e3d5958a30c816622
Marsel <marsiel.akhmetshin@gmail.com>
2019-02-20 00:03:01 +0500

    add selectionSort

--- selectionSort.js
+++ selectionSort.js
@@ -1,3 +1,19 @@
-function selectionSort(arr){
-    let key = arr[0]
+function selectionSort(arr) {
+  let min;
+
+  for (let i = 0; i < arr.length - 1; i++) {
+    min = i;
+    for (let j = i + 1; j < arr.length; j++) {
+      if (arr[j] < arr[min]) {
+        min = j;
+      }
+    }
+    let temp = arr[i];
+    arr[i] = arr[min];
+    arr[min] = temp;
+  }
+
+  return arr;
 }
+
+console.log(selectionSort([3, 2, 1, 4, 5, 33]));
